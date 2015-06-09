027eee059263799e0dbcc54295492b189bfe9a02
Sergio G <sergiorodriguezgijon@gmail.com>
2015-06-09 17:28:24 +0200

    Welcome match(es)

--- 3.guessing_game/guessing_game/src/main.rs
+++ 3.guessing_game/guessing_game/src/main.rs
@@ -1,12 +1,23 @@
+extern crate rand;
+
 use std::io;
+use rand::Rng;
+use std::cmp::Ordering;
 
 fn main() {
     println!("Guess the number I got in mind.");
     println!("-----------");
-
+    let number = rand::thread_rng().gen_range(1, 101);
     let mut guess = String::new();
     let mut console_input = io::stdin();
     console_input.read_line(&mut guess).ok().expect("OMFG guy, you force a panic. OMFG.");
 
-    println!("You said {}", guess);
+    let guess: u32 = guess.trim().parse().ok().expect("CMON man, A NUMBER. Friggin users..."); 
+
+    println!("You said {} and it was {}", guess, number);
+    match guess.cmp(&number) {
+      Ordering::Less => println!("Too small!"),
+      Ordering::Greater => println!("Too much!"),
+      Ordering::Equal => println!("You got this!"),
+    }
 }
