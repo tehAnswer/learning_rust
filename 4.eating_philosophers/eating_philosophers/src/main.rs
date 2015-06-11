06e58c9c760e34e7de4bac1a446a1e78b42d4747
Sergio G <sergiorodriguezgijon@gmail.com>
2015-06-11 14:54:06 +0200

    Solved problem HEUHEU

--- 4.eating_philosophers/eating_philosophers/src/main.rs
+++ 4.eating_philosophers/eating_philosophers/src/main.rs
@@ -1,16 +1,42 @@
 use std::thread;
+use std::sync::{Mutex, Arc};
 
 struct Philosopher {
-  name: String
+  name: String,
+  left: usize,
+  right: usize
+}
+
+struct Table {
+  forks: Vec<Mutex<()>>
+}
+
+impl Table {
+  fn new() -> Table {
+      Table {
+        forks: vec![
+          Mutex::new(()),
+          Mutex::new(()),
+          Mutex::new(()),
+          Mutex::new(()),
+          Mutex::new(())
+        ]
+      }
+  }
 }
 
 impl Philosopher {
-  fn new(name: &str) -> Philosopher {
+  fn new(name: &str, left: usize, right: usize) -> Philosopher {
     Philosopher {
-      name: name.to_string()
+      name: name.to_string(),
+      left: left,
+      right: right
     }
   }
-  fn eat(&self) {
+  fn eat(&self, table: &Table) {
+    let _left = table.forks[self.left].lock().unwrap();
+    let _right = table.forks[self.right].lock().unwrap();
+
     println!("{} is eating.", self.name);
     thread::sleep_ms(1000);
     println!("{} is done eating.", self.name);
@@ -19,28 +45,22 @@ impl Philosopher {
 
 fn main () {
   let philos = vec! [
-    Philosopher::new("Baruch Spinoza"),
-    Philosopher::new("Gilles Deleuze"),
-    Philosopher::new("Karl Marx"),
-    Philosopher::new("Friedrich Nietzsche"),
-    Philosopher::new("El Chanclas")
+    Philosopher::new("Baruch Spinoza", 0, 1),
+    Philosopher::new("Gilles Deleuze", 1, 2),
+    Philosopher::new("Karl Marx", 2, 3),
+    Philosopher::new("Friedrich Nietzsche", 3, 4),
+    Philosopher::new("El Chanclas", 0, 4) // el chanclas es zurdo.
   ];
 
-  println!("\nSecuential:");
-  println!("-------------\n");
-
-  for p in &philos {
-    p.eat()
-  }
-
-  thread::sleep_ms(500);
+  let table = Arc::new(Table::new());
 
   println!("\nMultithread:");
   println!("-------------\n");
 
   let handles: Vec<_> = philos.into_iter().map(|philo| {
+    let table = table.clone();
     thread::spawn(move || {
-      philo.eat();
+      philo.eat(&table);
     })
   }).collect();
 
