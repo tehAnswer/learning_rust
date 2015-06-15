0b3178dedc36a41374e869f569a102e015ab8fc0
Sergio G <sergiorodriguezgijon@gmail.com>
2015-06-15 12:40:10 +0200

    Idiomatic testing

--- 6.testing/adder/src/lib.rs
+++ 6.testing/adder/src/lib.rs
@@ -9,7 +9,16 @@ pub fn add_two(a: i32) -> i32 {
   a + 2
 }
 
-#[test]
-fn it_adds_correctly() {
-  assert_eq!(4, add_two(2));
+#[cfg(test)]
+mod tests {
+  use super::add_two;
+
+  #[test]
+  fn it_adds_correctly() {
+    assert_eq!(4, add_two(2));
+  }
 }
+
+
+
+
