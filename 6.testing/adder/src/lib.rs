6a83ca30cd03bc814e8ae016d90572a654affd4f
Sergio G <sergiorodriguezgijon@gmail.com>
2015-06-15 12:37:38 +0200

    Testing on public functions

--- 6.testing/adder/src/lib.rs
+++ 6.testing/adder/src/lib.rs
@@ -4,3 +4,12 @@ fn it_works() {
   assert!(false);
   assert_eq!("Hello", "world");
 }
+
+pub fn add_two(a: i32) -> i32 {
+  a + 2
+}
+
+#[test]
+fn it_adds_correctly() {
+  assert_eq!(4, add_two(2));
+}
